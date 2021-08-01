80ff430fe165d5b40c1d6dbc32f1817b4b2b57cf
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-01 21:50:45 +0300

    change backgroundimage

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const borderRadius = document.querySelector('.border-radius')
 const box = document.querySelector('.box')
-const offsetX = document.querySelector('.offsetX')
-const offsetY = document.querySelector('.offsetY')
-const blur = document.querySelector('.blur')
-const spread = document.querySelector('.spread')
+const offsetX = document.querySelector('.offsetX') //сдвиг по оси x
+const offsetY = document.querySelector('.offsetY') //сдвиг по оси y
+const blur = document.querySelector('.blur') //за размытие
+const spread = document.querySelector('.spread') //добавляет толщину
 const shadowColor = document.querySelector('.shadowColor')
-const colorBox = document.querySelector('.boxColor')
+const boxColor = document.querySelector('.boxColor')
 const out = document.querySelector('.out')
 const outset = document.querySelector('.outset')
 const inset = document.querySelector('.inset')
 
-borderRadius.oninput = cssGenerator
+borderRadius.oninput = cssGenerator //навешываем функцию
 offsetX.oninput = cssGenerator
 offsetY.oninput = cssGenerator
 blur.oninput = cssGenerator
 spread.oninput = cssGenerator
 shadowColor.oninput = cssGenerator
-colorBox.oninput = cssGenerator
+boxColor.oninput = cssGenerator
 out.oninput = cssGenerator
 outset.oninput = cssGenerator
 inset.oninput = cssGenerator
@@ -24,7 +24,7 @@ inset.oninput = cssGenerator
 function cssGenerator(){
     box.style.borderRadius = borderRadius.value + '%'
     box.style.boxShadow = `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${shadowColor.value}`
-    box.style.backgroundColor = `${colorBox.value} ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${shadowColor.value}`
+    box.style.backgroundColor = `${boxColor.value}`
 
     out.innerHTML = `Box-shadow:  ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${shadowColor.value} <br>`
     out.innerHTML += `- webkit-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${shadowColor.value} <br>`
